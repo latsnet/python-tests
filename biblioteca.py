a8395f0ee02634ee21b4264c0d2d8cdd439de44b
luizts <luiz.silveira@me.com.br>
2019-11-11 19:23:49 -0300

    Primeiros passos

---
+++ biblioteca.py
@@ -0,0 +1,13 @@
+def gera_nome_convite(convite):
+    posicao_final = len(convite)
+    posicao_inicial = posicao_final - 4
+    parte1 = convite[0:4]
+    parte2 = convite[posicao_inicial:posicao_final]
+    return parte1 + ' ' + parte2
+
+def envia_convite(convite):
+    print('Enviando convite para %s' % (convite))
+
+def processa_convite(convite):
+    nome_formatado = gera_nome_convite(convite)
+    envia_convite(nome_formatado)
