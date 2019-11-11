3a8c6e407e059b36e61779d9561008987fbdc012
luizts <luiz.silveira@me.com.br>
2019-11-11 20:05:28 -0300

    Novas funções de teste

--- biblioteca.py
+++ biblioteca.py
@@ -11,3 +11,17 @@ def envia_convite(convite):
 def processa_convite(convite):
     nome_formatado = gera_nome_convite(convite)
     envia_convite(nome_formatado)
+
+def calcula_idade():
+    ano_como_string = input("Digite o ano do seu nascimento: ")
+    ano_como_int = int(ano_como_string)
+    print("Sua idade é %s anos " % (2019 - ano_como_int))
+
+def cadastrar(nomes):
+    nome = input("Digite o seu nome: ")
+    nomes.append(nome)
+
+def remover(nomes):
+    print("Qual nome você gostaria de remover?")
+    nome = input()
+    nomes.remove(nome)
